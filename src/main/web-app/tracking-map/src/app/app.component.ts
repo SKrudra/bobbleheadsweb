803c36ead743b3dd77de4f018ccbbfa2c99676b1
Akash Solanki <akash.solanki@transplace.com>
2020-02-13 04:54:35 -0600

    Mapquest

--- src/main/web-app/tracking-map/src/app/app.component.ts
+++ src/main/web-app/tracking-map/src/app/app.component.ts
@@ -1,61 +1,36 @@
 import { Component } from '@angular/core';
 import { MouseEvent } from '@agm/core';
-
+declare var L : any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // google maps zoom level
-  zoom: number = 8;
+  private map: any;
 
-  // initial center position for the map
-  lat: number = 51.673858;
-  lng: number = 7.815982;
 
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
+  constructor() {
   }
 
-  mapClicked($event: MouseEvent) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
+
+  ngOnInit() {
+
+
+    L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';
+
+    this.map = L.mapquest.map('map', {
+
+      center: [37.7749, -122.4194],
+
+      layers: L.mapquest.tileLayer('map'),
+
+      zoom: 12
+
     });
+
+
+    this.map.addControl(L.mapquest.control());
+
   }
-
-  markerDragEnd(m: marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
-  }
-
-  markers: marker[] = [
-    {
-      lat: 51.673858,
-      lng: 7.815982,
-      label: 'A',
-      draggable: true
-    },
-    {
-      lat: 51.373858,
-      lng: 7.215982,
-      label: 'B',
-      draggable: false
-    },
-    {
-      lat: 51.723858,
-      lng: 7.895982,
-      label: 'C',
-      draggable: true
-    }
-  ]
-}
-
-// just an interface for type safety.
-interface marker {
-  lat: number;
-  lng: number;
-  label?: string;
-  draggable: boolean;
 }
--- src/main/web-app/tracking-map/src/app/app.module.ts
+++ src/main/web-app/tracking-map/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,11 +8,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AgmCoreModule.forRoot({
-      // please get your own API key here:
-      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
-      apiKey: 'AIzaSyAvcDy5ZYc2ujCS6TTtI3RYX5QmuoV8Ffw'
-    })
   ],
   providers: [],
   bootstrap: [AppComponent]
