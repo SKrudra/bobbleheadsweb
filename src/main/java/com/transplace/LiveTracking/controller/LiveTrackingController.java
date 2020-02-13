9f792e72bc1b4076bf9cf0f24feef04193c4e560
Nethre <psnethre@gmail.com>
2020-02-13 21:57:29 +0530

    BE: Added end points for Live Tracking

--- src/main/java/com/transplace/LiveTracking/controller/LiveTrackingController.java
+++ src/main/java/com/transplace/LiveTracking/controller/LiveTrackingController.java
@@ -1,9 +1,43 @@
 package com.transplace.LiveTracking.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.transplace.LiveTracking.model.CheckCall;
+import com.transplace.LiveTracking.model.ShipmentInfo;
+import com.transplace.LiveTracking.repo.CheckCallRepository;
+import com.transplace.LiveTracking.repo.ShipmentRepository;
 
 @RestController
 public class LiveTrackingController {
 
+	@Autowired
+	private ShipmentRepository shipmentRepo;
+	@Autowired
+	private CheckCallRepository checkCallRepository;
 	
+	@GetMapping("getshipmentDetails")
+	public ResponseEntity<ShipmentInfo> getshipmentDetails(@RequestParam("shipId") long shipmentId){
+		Optional<ShipmentInfo> shipmentInfo = shipmentRepo.findById(shipmentId);
+		
+		return new ResponseEntity<ShipmentInfo>(shipmentInfo.get(), HttpStatus.OK);
+	}
+	
+	@GetMapping("getCheckCalls")
+	public ResponseEntity<List<CheckCall>> getCheckCalls(@RequestParam("shipId") Long  shipmentId, @RequestParam(name="checkCallId",required=false) Long  checkCallId){
+		checkCallId=checkCallId==null?0:checkCallId;
+		Optional<List<CheckCall>> checkCalls = checkCallRepository.findByShipmentIdAndIdGreaterThan(shipmentId,checkCallId);
+		if(checkCalls.isPresent()) {
+			return new ResponseEntity<List<CheckCall>>(checkCalls.get(),HttpStatus.OK);
+		}
+		return new ResponseEntity<List<CheckCall>>(HttpStatus.NO_CONTENT);
+	}
 }
--- src/main/java/com/transplace/LiveTracking/repo/CheckCallRepository.java
+++ src/main/java/com/transplace/LiveTracking/repo/CheckCallRepository.java
@@ -1,5 +1,8 @@
 package com.transplace.LiveTracking.repo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import com.transplace.LiveTracking.model.CheckCall;
 @Repository
 public interface CheckCallRepository extends JpaRepository<CheckCall, Long> {
 
+	public Optional<List<CheckCall>> findByShipmentIdAndIdGreaterThan(long shipmentId,long checkCallId);
 }
